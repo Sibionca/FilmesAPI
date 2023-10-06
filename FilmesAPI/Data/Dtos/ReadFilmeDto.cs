c0aabb02fd4f2d4232700febf9c675c44923c3df
Sibionca <sibien28@gmail.com>
2023-10-05 22:18:07 -0300

    Adição do modelo e Dtos de cinema, controller e automap

--- FilmesAPI/Data/Dtos/ReadFilmeDto.cs
+++ FilmesAPI/Data/Dtos/ReadFilmeDto.cs
@@ -1,11 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace FilmesAPI.Data;
+namespace FilmesAPI.Data.Dtos;
 
 public class ReadFilmeDto
 {
     public string Titulo { get; set; }
     public string Genero { get; set; }
     public int Duracao { get; set; }
-    public DateTime DataDaConsulta { get; set;} = DateTime.Now;
+    public DateTime DataDaConsulta { get; set; } = DateTime.Now;
 }
--- FilmesAPI/Profiles/FilmeProfile.cs
+++ FilmesAPI/Profiles/FilmeProfile.cs
@@ -1,5 +1,5 @@
 ﻿using AutoMapper;
-using FilmesAPI.Data;
+using FilmesAPI.Data.Dtos;
 using FilmesAPI.Models;
 
 namespace FilmesAPI.Profiles;
